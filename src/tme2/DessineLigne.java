2e313eb7239c32f867bb0a7701579dad10170dca
Thomas <thomas.zouba@etu.sorbonne-universite.fr>
2021-09-29 17:11:12 +0200

    question 2

---
+++ src/tme2/DessineLigne.java
@@ -0,0 +1,23 @@
+package tme2;
+import java.awt.Point;
+import graphic.Window;
+
+public class DessineLigne extends Thread {
+	
+	private int x1,y1, x2,y2;
+	private Window w;
+	
+	
+	
+	public DessineLigne(int ax, int ay, int bx, int by, Window wi){
+		x1 = ax; 
+		x2 = bx; 
+		y1 = ay; 
+		y2 = by;
+		w = wi;
+	}
+	
+	public void run() {
+		w.drawLine(new Point(x1, y1) ,new Point(x2, y2) );
+	}
+}
